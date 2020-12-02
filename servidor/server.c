3bec07d326751bd3e315e8c459722b579a2c8c17
paulorivera1992 <55298451+Paulorivera1992@users.noreply.github.com>
2020-12-01 22:49:04 -0300

    actualizacion 01-12

--- servidor/server.c
+++ servidor/server.c
@@ -193,7 +193,187 @@ static void Definir_Sensor_type(UA_Server *server) {
       UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10010),
                            UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
                            UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true);    
-
+    
+    //se crea una variable asociada al SensorType que guarda la lectura de media de soot propensity                         
+    UA_VariableAttributes AVG_STAttr = UA_VariableAttributes_default;
+    AVG_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor de la media de soot propensity");
+    AVG_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "avg_soot_propensity");
+    AVG_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_AVG_ST = 0;
+    UA_Variant_setScalar(&AVG_STAttr.value, &Valor_AVG_ST, &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10011),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "Avg_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE),AVG_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10011),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+    
+    //se crea una variable asociada al SensorType que guarda la lectura de mediana de soot propensity                         
+    UA_VariableAttributes MEDIAN_STAttr = UA_VariableAttributes_default;
+    MEDIAN_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor de la mediana de soot propensity");
+    MEDIAN_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "median_soot_propensity");
+    MEDIAN_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_MEDIAN_ST = 0;
+    UA_Variant_setScalar(&MEDIAN_STAttr.value, &Valor_MEDIAN_ST , &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10012),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "Median_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), MEDIAN_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10012),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+    
+    //se crea una variable asociada al SensorType que guarda la lectura de desviacion estandar de soot propensity                         
+    UA_VariableAttributes STD_STAttr = UA_VariableAttributes_default;
+    STD_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor de la desviacion estandar de soot propensity");
+    STD_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "std_soot_propensity");
+    STD_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_STD_ST = 0;
+    UA_Variant_setScalar(&STD_STAttr.value, &Valor_STD_ST , &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10013),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "Std_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), STD_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10013),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true);
+    
+    //se crea una variable asociada al SensorType que guarda la lectura de la moda de soot propensity                         
+    UA_VariableAttributes MODE_STAttr = UA_VariableAttributes_default;
+    MODE_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor de la moda de soot propensity");
+    MODE_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "mode_soot_propensity");
+    MODE_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_MODE_ST = 0;
+    UA_Variant_setScalar(&MODE_STAttr.value, &Valor_MODE_ST, &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10014),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "Mode_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), MODE_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10014),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+                           
+    //se crea una variable asociada al SensorType que guarda la lectura del percentil 50 de soot propensity                         
+    UA_VariableAttributes P50_STAttr = UA_VariableAttributes_default;
+    P50_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor del percentil 50 de soot propensity");
+    P50_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "P50_soot_propensity");
+    P50_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_P50_ST = 0;
+    UA_Variant_setScalar(&P50_STAttr.value, &Valor_P50_ST , &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10015),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "P50_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), P50_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10015),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+    
+    //se crea una variable asociada al SensorType que guarda la lectura del segundo momento de soot propensity                         
+    UA_VariableAttributes MOM2_STAttr = UA_VariableAttributes_default;
+    MOM2_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor del segundo momento de soot propensity");
+    MOM2_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "MOM2_soot_propensity");
+    MOM2_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_MOM2_ST = 0;
+    UA_Variant_setScalar(&MOM2_STAttr.value, &Valor_MOM2_ST , &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10016),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "MOM2_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), MOM2_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10016),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+                           
+    //se crea una variable asociada al SensorType que guarda la lectura del tercer momento de soot propensity                         
+    UA_VariableAttributes MOM3_STAttr = UA_VariableAttributes_default;
+    MOM3_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor del tercer momento de soot propensity");
+    MOM3_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "MOM3_soot_propensity");
+    MOM3_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_MOM3_ST = 0;
+    UA_Variant_setScalar(&MOM3_STAttr.value, &Valor_MOM3_ST , &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10017),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "MOM3_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), MOM3_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10017),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+    
+    //se crea una variable asociada al SensorType que guarda la lectura del cuarto momento de soot propensity                         
+    UA_VariableAttributes MOM4_STAttr = UA_VariableAttributes_default;
+    MOM4_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor del cuarto momento de soot propensity");
+    MOM4_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "MOM4_soot_propensity");
+    MOM4_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_MOM4_ST = 0;
+    UA_Variant_setScalar(&MOM4_STAttr.value, &Valor_MOM4_ST , &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10018),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "MOM4_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), MOM4_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10018),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+    
+    //se crea una variable asociada al SensorType que guarda la lectura del primer coeficiente de asimetria de soot propensity                         
+    UA_VariableAttributes SKEW1_STAttr = UA_VariableAttributes_default;
+    SKEW1_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor del primer coeficiente de asimetria de soot propensity");
+    SKEW1_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "skew1_soot_propensity");
+    SKEW1_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_SKEW1_ST = 0;
+    UA_Variant_setScalar(&SKEW1_STAttr.value, &Valor_SKEW1_ST , &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10019),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "Skew1_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), SKEW1_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10019),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+    
+    //se crea una variable asociada al SensorType que guarda la lectura del segundo coeficiente de asimetria de soot propensity                         
+    UA_VariableAttributes SKEW2_STAttr = UA_VariableAttributes_default;
+    SKEW2_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor del segundo coeficiente de asimetria de soot propensity");
+    SKEW2_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "skew2_soot_propensity");
+    SKEW2_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_SKEW2_ST = 0;
+    UA_Variant_setScalar(&SKEW2_STAttr.value, &Valor_SKEW2_ST , &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10020),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "Skew2_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), SKEW2_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10020),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+    
+    //se crea una variable asociada al SensorType que guarda la lectura del coeficiente de asimetria de soot propensity                         
+    UA_VariableAttributes SKEW3_STAttr = UA_VariableAttributes_default;
+    SKEW3_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor del coeficiente de asimetria de soot propensity");
+    SKEW3_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "skew3_soot_propensity");
+    SKEW3_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_SKEW3_ST = 0;
+    UA_Variant_setScalar(&SKEW3_STAttr.value, &Valor_SKEW3_ST , &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10021),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "Skew3_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), SKEW3_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10021),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+    
+    //se crea una variable asociada al SensorType que guarda la lectura del coeficiente de curtosis de soot propensity                         
+    UA_VariableAttributes KURT_STAttr = UA_VariableAttributes_default;
+    KURT_STAttr.description =  UA_LOCALIZEDTEXT("en-US", "Este es el valor del coeficiente de asimetria de soot propensity");
+    KURT_STAttr.displayName =  UA_LOCALIZEDTEXT("en-US", "kurt_soot_propensity");
+    KURT_STAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_UInt32 Valor_KURT_ST = 0;
+    UA_Variant_setScalar(&KURT_STAttr.value, &Valor_KURT_ST , &UA_TYPES[UA_TYPES_UINT32]);
+    UA_Server_addVariableNode(server, UA_NODEID_NUMERIC(1, 10022),
+                              UA_NODEID_NUMERIC(1, 10000), UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),
+                              UA_QUALIFIEDNAME(1, "Kurt_soot_propensity"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), KURT_STAttr, NULL, NULL);
+                              
+    UA_Server_addReference(server, UA_NODEID_NUMERIC(1, 10022),
+                           UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+                           UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true); 
+    
 }
 
 /*******************funcion para crear un nuevo tipo de objetos, Sensor_Type **************************/
